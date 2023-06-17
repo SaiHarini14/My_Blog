ed9f70daf011e136f08cb7447579d930dab64387
SaiHarini14 <saiharini.g1484@gmail.com>
2023-06-17 23:33:31 +0530

    Initial

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<title>MY BLOG -FLOWERS</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+            <?= $post ?>
+        </article>
+    <a href="/">GO BACK</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<title>MY BLOG -FLOWERS</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/posts/daisy" >DAISY <a></h1>
+        <p>
+            <p>
+                The daisy is a common type of flower that belongs to the Asteraceae family, which also includes sunflowers, dandelions, and asters. Daisies are known for their delicate and simple beauty, characterized by a central disc surrounded by ray-like petals. They are widespread and can be found in various regions around the world.
+    
+    
+                Here are some key features and information about daisies:
+    
+    
+    
+                1. Appearance: Daisies typically have a yellow disc-shaped center, known as the "disc floret," which is composed of many tiny individual flowers called florets. Surrounding the disc floret, there is a ring of elongated, white petals known as "ray florets." However, daisies can also come in various colors such as pink, red, orange, and even combinations of colors.
+    
+    
+    
+                2. Types of Daisies: There are numerous species and varieties of daisies, but one of the most well-known is the common daisy (Bellis perennis). Other popular daisy species include the English daisy (Bellis perennis), African daisy (Osteospermum), Gerbera daisy (Gerbera), and Shasta daisy (Leucanthemum x superbum).
+    
+    
+    
+                3. Symbolism: Daisies are often associated with innocence, purity, and simplicity. They are frequently used in floral arrangements and bouquets for their fresh and cheerful appearance. Daisies are also commonly associated with childhood and are often depicted in children's literature and artwork.
+    
+    
+    
+                4. Cultivation: Daisies are relatively easy to grow and are a popular choice for gardeners. They thrive in areas with full sun or partial shade and well-drained soil. Daisies can be propagated from seeds or by dividing mature plants. They are considered perennials in many regions, meaning they come back year after year.
+    
+    
+    
+                5. Cultural Significance: Daisies have held symbolic meaning in various cultures throughout history. For example, in ancient Roman mythology, the daisy was associated with the goddess Venus and symbolized love and fertility. In some traditions, daisies were used for medicinal purposes to treat ailments like stomachaches and wounds.
+    
+    
+    
+                6. Usage: Apart from their ornamental value, daisies have some practical uses as well. Some species of daisies, such as the chamomile daisy (Matricaria chamomilla), have been used for their medicinal properties. Chamomile tea, made from the dried flowers, is known for its soothing and calming effects.
+    
+    
+    
+                7. Environmental Importance: Daisies, like other flowering plants, play a vital role in pollination. They attract bees, butterflies, and other insects, which help in the transfer of pollen and the fertilization of the flowers. This interaction is crucial for the reproduction and survival of many plant species, including daisies.
+    
+    
+    
+                Whether they're growing wild in a meadow or adorning a carefully cultivated garden, daisies are cherished for their timeless beauty and gentle charm.
+    
+            </p>
+
+        </p>
+    </article>
+
+
+    <article>
+        <h1>><a href="/posts/rose" >ROSE <a> </h2>
+            <p>
+                Roses are one of the most beloved and widely recognized flowers in the world. They are part of the Rosaceae family and are known for their exquisite beauty, fragrance, and symbolic significance. Here is some information about roses:
+
+
+                1. Appearance: Roses are known for their large, showy blooms with layers of petals that come in a wide range of colors. The petals can be found in shades of red, pink, white, yellow, orange, and even purple. The flowers can be single with a few petals or fully double with many layers of petals, depending on the variety.
+
+
+                
+                2. Types of Roses: There are thousands of rose varieties available, each with its own unique characteristics. Some popular types include hybrid tea roses, which are known for their classic, high-centered blooms; floribunda roses, which produce clusters of smaller flowers; grandiflora roses, which have large blooms on tall stems; and climbing roses, which have long canes that can be trained to grow on walls, fences, or trellises.
+
+
+                
+                3. Symbolism: Roses are often associated with love, beauty, and romance. Different colors of roses convey various meanings. Red roses traditionally symbolize passionate love, while pink roses represent admiration and gratitude. White roses are often associated with purity and innocence, while yellow roses are often given as a symbol of friendship and joy. Roses have been used throughout history to convey emotions and sentiments.
+
+
+                
+                4. Cultivation: Roses can be grown in various climates, but they generally prefer well-drained soil, ample sunlight, and regular watering. They are often planted in gardens, but they can also be grown in containers. Proper pruning and care are necessary to maintain healthy and productive rose bushes.
+
+
+                
+                5. Cultural Significance: Roses hold significant cultural and historical importance. They have been cultivated for thousands of years and have appeared in art, literature, and religious symbolism. For example, roses were highly valued in ancient Greece and Rome and were associated with goddesses such as Aphrodite and Venus. Roses are also often used in weddings, anniversaries, and other special occasions to symbolize love and celebration.
+
+
+                
+                6. Fragrance and Essential Oils: Many rose varieties are highly fragrant, and their scent is used in perfumes, soaps, and other beauty products. Rose essential oil, obtained through steam distillation of rose petals, is prized for its therapeutic properties and is used in aromatherapy for relaxation and promoting emotional well-being.
+
+
+                
+                7. Commercial Importance: Roses are commercially significant as cut flowers and as potted plants. They are popular choices for floral arrangements, bouquets, and gifts. The global rose industry plays a major role in horticulture and contributes to the economy of many countries.
+
+
+                
+                Roses have an enduring appeal that transcends cultures and time. They continue to captivate people with their beauty, fragrance, and symbolic meanings, making them a cherished flower around the world.
+
+            </p>
+        </article>
+
+        <article>
+        <h1>><a href="/posts/sunflower" >SUNFLOWER <a></h1>
+        <p>
+            Sunflowers are vibrant and iconic flowers that are widely recognized for their large, yellow blooms and tall stature. Here's some information about sunflowers:
+
+
+
+            1. Appearance: Sunflowers (scientifically known as Helianthus annuus) are known for their striking appearance. They feature a large, round central disk that is composed of tiny individual flowers, surrounded by bright yellow or golden petals. The central disk appears as a spiral pattern, giving the flower its unique aesthetic. Sunflowers can grow to impressive heights, with some varieties reaching up to 10 feet (3 meters) or more.
+
+
+
+            2. Sunflower Varieties: There are different varieties of sunflowers, including those with single or multiple blooms, and variations in petal colors. While the classic sunflower has yellow petals, there are also cultivars with red, orange, burgundy, or even bi-colored petals. Some varieties are bred for specific characteristics, such as dwarf sunflowers for container gardening or pollen-free sunflowers for those with allergies.
+
+
+
+            3. Growth and Cultivation: Sunflowers are annual plants, meaning they complete their life cycle within a single year. They are relatively easy to grow and are popular in gardens, fields, and even as potted plants. Sunflowers prefer full sun exposure and well-drained soil. They have a deep root system that helps them tolerate drought conditions.
+
+
+
+            4. Symbolism and Meaning: Sunflowers are often associated with positive meanings such as happiness, warmth, and adoration. Their name comes from the way they turn their blooms to face the sun, following its movement throughout the day. This behavior, known as heliotropism, symbolizes loyalty and a vibrant, sunny disposition.
+
+
+
+            5. Agricultural and Commercial Importance: Sunflowers are not only admired for their beauty but also have various practical uses. They are cultivated as oilseed crops, primarily for their nutritious seeds, which are rich in healthy fats and proteins. Sunflower oil is widely used in cooking and as an ingredient in many food products. Additionally, sunflower seeds are enjoyed as a snack and are also a popular ingredient in bird feed.
+
+
+
+            6. Wildlife and Ecological Value: Sunflowers have ecological importance as they attract a variety of pollinators, including bees and butterflies, which aid in the pollination of other plants. Sunflowers also provide food and habitat for birds, especially during the fall when the seeds mature. The plants can create a beautiful and beneficial ecosystem in gardens, fields, or natural areas.
+
+
+
+            7. Cultural Significance: Sunflowers hold cultural significance in various societies. They have been depicted in art, literature, and folklore, symbolizing happiness, fertility, and the beauty of nature. Sunflowers have inspired numerous artists, including Vincent van Gogh, who famously painted a series of sunflower artworks.
+
+
+
+            Sunflowers are cherished for their vibrant beauty, symbolism, and practical uses. Their cheerful appearance and sunny disposition continue to captivate people, making them a beloved flower around the world.
+
+        </p>
+    </article>
+</body>
